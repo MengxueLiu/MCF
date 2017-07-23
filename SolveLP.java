4b6e02281a94218e2b6ba0944ee9b6f68d955088
MengxueLiu <Mengxue.Liu@asu.edu>
2017-07-23 11:47:00 -0700

    Add files via upload

--- SolveLP.java
+++ SolveLP.java
@@ -2,18 +2,15 @@ import gurobi.*;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class SolveLP {
 
     public static void main(String[] args) throws Exception {
 
-        String fileName = "Graph2";
+        String fileName = "Graph";
         float fileSize = 200;
-        int numOfCommodities = 1;
+        int numOfCommodities = 2;
 
 
         List<String[]> Capacities = new ArrayList<>();
@@ -21,6 +18,7 @@ public class SolveLP {
         HashMap<String, List<String>> adj_list_in = new HashMap<>();
         List<GRBVar> flow_variables = new ArrayList<>();
         HashMap<String, Integer> mapping = new HashMap<>();
+        Set<String> nodes = new HashSet<>();
 
 
         // Model
@@ -28,9 +26,8 @@ public class SolveLP {
         GRBModel model = new GRBModel(env);
 
         String[] source_sink = new String[numOfCommodities];
-        source_sink[0] = "1,4";
-//        source_sink[1] = "2,12";
-
+        source_sink[0] = "1,10";
+        source_sink[1] = "2,12";
 
 
         BufferedReader BR = new BufferedReader(new FileReader(fileName));
@@ -42,7 +39,13 @@ public class SolveLP {
             String n1 = tmp[0];
             String n2 = tmp[1];
 
-            Capacities.add(new String[]{n1, n2, ""+Integer.parseInt(tmp[2])/fileSize});
+            if (!nodes.contains(n1))
+                nodes.add(n1);
+
+            if (!nodes.contains(n2))
+                nodes.add(n2);
+
+            Capacities.add(new String[]{n1, n2, "" + Integer.parseInt(tmp[2]) / fileSize});
 
 
             if (adj_list_out.containsKey(n1))
@@ -64,9 +67,9 @@ public class SolveLP {
 
 
             for (int p = 0; p < numOfCommodities; ++p) {
-                GRBVar tmp_flow = model.addVar(0, 1, 0, GRB.CONTINUOUS, "f_"+n1+"-"+n2+"-"+p);
+                GRBVar tmp_flow = model.addVar(0, 1, 0, GRB.CONTINUOUS, "f_" + n1 + "-" + n2 + "-" + p);
                 flow_variables.add(tmp_flow);
-                mapping.put("f_"+n1+"-"+n2+"-"+p, flow_variables.size()-1);
+                mapping.put("f_" + n1 + "-" + n2 + "-" + p, flow_variables.size() - 1);
             }
         }
 
@@ -74,7 +77,7 @@ public class SolveLP {
 
         GRBVar[] flows = new GRBVar[2];
         for (int p = 0; p < numOfCommodities; ++p) {
-            flows[p] = model.addVar(0, 1, 0, GRB.CONTINUOUS, "f2_"+p);
+            flows[p] = model.addVar(0, 1, 0, GRB.CONTINUOUS, "f2_" + p);
         }
 
 
@@ -83,8 +86,7 @@ public class SolveLP {
         // Objective Function
         GRBLinExpr expr = new GRBLinExpr();
         for (int p = 0; p < numOfCommodities; ++p) {
-            expr.addTerm(1.0, model.getVarByName("f2_"+p));
-//            expr.addTerm(1.0, flows[p]);
+            expr.addTerm(1.0, model.getVarByName("f2_" + p));
 
         }
         model.setObjective(expr, GRB.MAXIMIZE);
@@ -98,13 +100,11 @@ public class SolveLP {
             List<String> neighbors = adj_list_out.get(source);
 
             GRBLinExpr expr2 = new GRBLinExpr();
-            for(int j = 0; j != neighbors.size(); j++) {
-                expr2.addTerm(1.0, model.getVarByName("f_"+source+"-"+neighbors.get(j)+"-"+i));
-//                expr2.addTerm(1.0, flow_variables.get(mapping.get("f_"+source+"-"+neighbors.get(j)+"-"+i)));
+            for (int j = 0; j != neighbors.size(); j++) {
+                expr2.addTerm(1.0, model.getVarByName("f_" + source + "-" + neighbors.get(j) + "-" + i));
             }
 
-            model.addConstr(expr2, GRB.EQUAL, model.getVarByName("f2_"+i), "c0_"+i);
-//            model.addConstr(expr2, GRB.EQUAL, flows[i], "c0_"+i);
+            model.addConstr(expr2, GRB.EQUAL, model.getVarByName("f2_" + i), "c0_" + i);
         }
 
         model.update();
@@ -113,33 +113,50 @@ public class SolveLP {
         // Add constraint 1
         for (int i = 0; i != numOfCommodities; i++) {
 
-            GRBLinExpr expr_inFlow = new GRBLinExpr();
-            GRBLinExpr expr_outFlow = new GRBLinExpr();
             String currentCommodity_source = source_sink[i].split(",")[0];
             String currentCommodity_sink = source_sink[i].split(",")[1];
 
-            for (Map.Entry<String, List<String>> entry : adj_list_out.entrySet()) {
-                String current = entry.getKey();
-                if (current.equals(currentCommodity_source) || current.equals(currentCommodity_sink) || !adj_list_in.containsKey(current))
+            for (String current : nodes) {
+                if (current.equals(currentCommodity_source) || current.equals(currentCommodity_sink))
                     continue;
 
-                List<String> tmp_outFlow = entry.getValue();
-                for (int k = 0; k != tmp_outFlow.size(); k++) {
-                    String to = tmp_outFlow.get(k);
-                    expr_outFlow.addTerm(1.0, model.getVarByName("f_"+current+"-"+to+"-"+i));
-//                    expr_outFlow.addTerm(1.0, flow_variables.get(mapping.get("f_"+current+"-"+to+"-"+i)));
-                }
+                if (adj_list_in.containsKey(current) && adj_list_out.containsKey(current)) {
+                    GRBLinExpr expr_inFlow = new GRBLinExpr();
+                    GRBLinExpr expr_outFlow = new GRBLinExpr();
 
-                List<String> tmp_inFlow = adj_list_in.get(current);
-                for (int k = 0; k != tmp_inFlow.size(); k++) {
-                    String from = tmp_inFlow.get(k);
-                    expr_inFlow.addTerm(1.0, model.getVarByName("f_"+from+"-"+current+"-"+i));
-//                    expr_outFlow.addTerm(1.0, flow_variables.get(mapping.get("f_"+from+"-"+current+"-"+i)));
+                    List<String> tmp_outFlow = adj_list_out.get(current);
+                    for (int k = 0; k != tmp_outFlow.size(); k++) {
+                        String to = tmp_outFlow.get(k);
+                        expr_outFlow.addTerm(1.0, model.getVarByName("f_" + current + "-" + to + "-" + i));
+                    }
+
+                    List<String> tmp_inFlow = adj_list_in.get(current);
+                    for (int k = 0; k != tmp_inFlow.size(); k++) {
+                        String from = tmp_inFlow.get(k);
+                        expr_inFlow.addTerm(1.0, model.getVarByName("f_" + from + "-" + current + "-" + i));
+                    }
+                    model.addConstr(expr_inFlow, GRB.EQUAL, expr_outFlow, "c1_" + current + "-" + i);
+
+                } else if (adj_list_in.containsKey(current) && !adj_list_out.containsKey(current)) {
+                    GRBLinExpr expr_inFlow = new GRBLinExpr();
+                    List<String> tmp_inFlow = adj_list_in.get(current);
+                    for (int k = 0; k != tmp_inFlow.size(); k++) {
+                        String from = tmp_inFlow.get(k);
+                        expr_inFlow.addTerm(1.0, model.getVarByName("f_" + from + "-" + current + "-" + i));
+                    }
+                    model.addConstr(expr_inFlow, GRB.EQUAL, 0, "c1_" + current + "-" + i);
+
+                } else if (!adj_list_in.containsKey(current) && adj_list_out.containsKey(current)) {
+                    GRBLinExpr expr_outFlow = new GRBLinExpr();
+                    List<String> tmp_outFlow = adj_list_out.get(current);
+                    for (int k = 0; k != tmp_outFlow.size(); k++) {
+                        String to = tmp_outFlow.get(k);
+                        expr_outFlow.addTerm(1.0, model.getVarByName("f_" + current + "-" + to + "-" + i));
+                    }
+                    model.addConstr(expr_outFlow, GRB.EQUAL, 0, "c1_" + current + "-" + i);
                 }
 
             }
-
-            model.addConstr(expr_inFlow, GRB.EQUAL, expr_outFlow, "c1_"+i);
 
         }
 
@@ -155,15 +172,16 @@ public class SolveLP {
 
             GRBLinExpr expr_edgeFlow = new GRBLinExpr();
             for (int i = 0; i != numOfCommodities; i++) {
-                expr_edgeFlow.addTerm(1.0, model.getVarByName("f_"+n1+"-"+n2+"-"+i));
-//                expr_edgeFlow.addTerm(1.0, flow_variables.get(mapping.get("f_"+n1+"-"+n2+"-"+i)));
+                expr_edgeFlow.addTerm(1.0, model.getVarByName("f_" + n1 + "-" + n2 + "-" + i));
             }
 
-            model.addConstr(expr_edgeFlow, GRB.LESS_EQUAL, Float.parseFloat(cap), "c2_"+k);
+            model.addConstr(expr_edgeFlow, GRB.LESS_EQUAL, Float.parseFloat(cap), "c2_" + k);
 
         }
 
         model.update();
+
+        model.write("mcf_debug.lp");
 
         model.optimize();
         System.out.println("Obj: " + model.get(GRB.DoubleAttr.ObjVal));
